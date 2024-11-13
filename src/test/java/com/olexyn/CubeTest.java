68e9e84fca7f1340073c4aa3d69752920fd5e6a0
io42630 <ivan@olexyn.com>
2024-11-13 13:13:13 +0100

    fix: reduce test size

--- src/test/java/com/olexyn/CubeTest.java
+++ src/test/java/com/olexyn/CubeTest.java
@@ -4,21 +4,18 @@ import org.junit.Test;
 
 public class CubeTest {
 
+	@Test
+	public void makeCube() {
 
+		int side = 180;
+		long maxLong = Long.MAX_VALUE;
+		long restLong = maxLong / side / side / side;
 
-    @Test
-    public void makeCube() {
+		int maxInt = Integer.MAX_VALUE;
+		int restInt = maxInt / side / side / side;
 
-        int side = 1800;
-        long maxLong = Long.MAX_VALUE;
-        long restLong = maxLong / side / side / side;
+		var cube1 = new InstantCube(side, 3);
 
-        int maxInt = Integer.MAX_VALUE;
-        int restInt = maxInt / side / side / side;
-
-
-        var cube1 = new InstantCube(side, 3);
-
-        int br = 0;
-    }
+		int br = 0;
+	}
 }
